79dc203788c5fc911aa0ee73eb573789db948a00
aliasici <asiciali@yahoo.com>
2020-11-14 21:38:07 -0800

    I added some code into GoogleSearchDefinitions.java class

--- src/test/java/com/aliasici/step_definitions/GoogleSearchDefinitions.java
+++ src/test/java/com/aliasici/step_definitions/GoogleSearchDefinitions.java
@@ -5,6 +5,7 @@ import com.aliasici.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -26,28 +27,36 @@ public class GoogleSearchDefinitions {
     @Given("user is on google search page")
     public void user_is_on_google_search_page() {
         System.out.println("on google");
-        WebElement searchBox = Driver.getDriver().findElement(By.name("q"));
-        String search = ConfigurationReader.getProperty("searchValue");
+      //  WebElement googleHome = Driver.getDriver().findElement(By.className("ddlv-a"));
+        //expected
+        String expectedInTitle = "Google";
+        //actual
+        String actualTitle = Driver.getDriver().getTitle();
 
-        searchBox.sendKeys("apple watch" + Keys.ENTER);
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
 
     }
 
     @When("user enters a text in search box")
-    public void user_enters_a_text_in_search_box() {
+    public void user_enters_a_text_in_search_box() throws InterruptedException {
         System.out.println("enter keyword");
+        Driver.getDriver().findElement(By.name("q")).sendKeys("apple watch");
+        Thread.sleep(2000);
 
     }
 
     @When("hits enter")
     public void hits_enter() {
         System.out.println("hit search button");
+        Driver.getDriver().findElement(By.name("q")).sendKeys(Keys.ENTER);
 
     }
 
     @Then("user is navigated to search results")
     public void user_is_navigated_to_search_results() {
         System.out.println("see whatever enter");
+        Driver.getDriver().getTitle().contains("Apple Watch");
+
 
     }
 
